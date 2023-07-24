0e9ac28007c80b3e37954faf2e6a95296912c73a
Talanova <ntalanov@its.jnj.com>
2023-07-25 00:43:58 +0300

    Seminar1 Task3

---
+++ Seminar1/Task3/Program.cs
@@ -0,0 +1,55 @@
+﻿// Напишите программу, которая будет выдавать название дня недели по заданному номеру.
+// 3 -> Среда
+// 5 -> Пятница
+
+Console.Write("Введите число от 1 до 7: ");
+int day = int.Parse(Console.ReadLine());
+if(day == 1)
+{
+    Console.Write("Понедельник");
+}
+else{
+    if(day == 2)
+    {
+        Console.Write("Вторник");
+    }
+    else
+    {
+        if (day == 3)
+        {
+            Console.Write("Среда");
+        }
+        else
+        {
+            if (day == 4)
+            {
+                Console.Write("Четверг");
+            }
+            else
+            {
+                if (day == 5)
+                {
+                    Console.Write("Пятница");
+                }
+                else
+                {
+                    if (day == 6)
+                    {
+                        Console.Write("Суббота");
+                    }
+                    else
+                    {
+                        if (day == 7)
+                        {
+                            Console.Write("Воскресенье");
+                        }
+                        else
+                        {
+                            Console.Write("Неверное число");
+                        }
+                    }
+                }
+            }
+        }
+    }
+}
