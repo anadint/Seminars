02e1d6b62feb95cedd14eb1ea0fdfdb364ed748c
Talanova <ntalanov@its.jnj.com>
2023-07-26 23:14:26 +0300

    Seminar3 Task2

---
+++ Seminar3/Task2/Program.cs
@@ -0,0 +1,40 @@
+﻿// Напишите программу, которая по заданному номеру четверти, показывает диапазон возможных координат точек в этой четверти (x и y)
+
+string CoordinateRange(int quarter)
+{
+    string xRange = "";
+    string yRange = "";
+    if (quarter == 1)
+    {
+        xRange = "x > 0";
+        yRange = " y > 0";
+    }
+    if (quarter == 2)
+    {
+        xRange = "x < 0";
+        yRange = " y > 0";
+    }
+    if (quarter == 3)
+    {
+        xRange = "x < 0";
+        yRange = " y < 0";
+    } 
+    if (quarter == 4)
+    {
+        xRange = "x > 0";
+        yRange = " y < 0";
+    }
+    return xRange +  yRange;
+    
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    int numQuarter = Convert.ToInt32(Console.ReadLine());
+    return numQuarter;
+
+}
+
+int quarter = GetInfo("Введите номер четверти: ");
+Console.WriteLine($"Диапазон {CoordinateRange(quarter)}");
