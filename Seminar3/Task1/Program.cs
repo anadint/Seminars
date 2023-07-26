bcf152d1495a30335d13239f0b5564eb1774c578
Talanova <ntalanov@its.jnj.com>
2023-07-26 22:32:40 +0300

    Seminar3 Task1

--- Seminar3/Task1/Program.cs
+++ Seminar3/Task1/Program.cs
@@ -1,2 +1,23 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача №17. Напишите программу, которая принимает на вход координаты точки (X и Y),
+// причем X ≠ 0 и Y ≠ 0 и выдаёт номер четверти плоскости, в которой находится эта точка.
+
+int DefQuarter(int xPoint, int yPoint)
+{
+    if (xPoint > 0 & yPoint > 0) return 1;
+    if (xPoint < 0 & yPoint > 0) return 2;
+    if (xPoint < 0 & yPoint < 0) return 3;
+    if (xPoint > 0 & yPoint < 0) return 4;
+    return 0;
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    int inputNum = Convert.ToInt32(Console.ReadLine());
+    return inputNum;
+
+}
+
+int xPoint = GetInfo("Введите координату X: ");
+int yPoint = GetInfo("Введите координату Y: ");
+Console.WriteLine($"Это {DefQuarter(xPoint, yPoint)} четверть!");
