3006dc36b17c38a4f48daec000fd6229bd816c1a
Talanova <ntalanov@its.jnj.com>
2023-07-26 23:56:29 +0300

    Seminar3 Task4

---
+++ Seminar3/Task4/Program.cs
@@ -0,0 +1,26 @@
+﻿// Напишите программу, которая принимает на вход число (N) и выдаёт таблицу квадратов чисел
+// от 1 до N.
+
+// 5 -> 1, 4, 9, 16, 25.
+// 2 -> 1,4
+
+void DefSqr(int num)
+{
+    Console.Write($"{num} -> ");
+    for (int i = 1; i < num; i++)
+    {
+        Console.Write(i * i + ", ");
+    }
+    Console.WriteLine(num * num + "."); 
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    int inputNum = Convert.ToInt32(Console.ReadLine());
+    return inputNum;
+
+}
+
+int num = GetInfo("Введите число: ");
+DefSqr(num);
