9633616e2a4d5b32abc82ca876a0f7cc2da8356e
Talanova <ntalanov@its.jnj.com>
2023-07-26 23:28:41 +0300

    Seminar3 Task3

---
+++ Seminar3/Task3/Program.cs
@@ -0,0 +1,24 @@
+﻿// Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+
+// A (3,6); B (2,1) -> 5,09
+// A (7,-5); B (1,-1) -> 7,21
+
+double Length(int xA, int yA, int xB, int yB)
+{
+    double distance = Math.Sqrt(Math.Pow(xB - xA, 2) + Math.Pow(yB - yA, 2));
+    return distance;
+}
+
+int GetInfo(string message)
+{
+    Console.Write(message);
+    int inputNum = Convert.ToInt32(Console.ReadLine());
+    return inputNum;
+
+}
+
+int xPointA = GetInfo("Введите координату X точки А: ");
+int yPointA = GetInfo("Введите координату Y точки А: ");
+int xPointB = GetInfo("Введите координату X точки B: ");
+int yPointB = GetInfo("Введите координату Y точки B: ");
+Console.WriteLine($"Расстояние между точками А и В равно {Length(xPointA, yPointA,xPointB, yPointB)}");
