4540ecda83b6e11b53cafe9d5a934a36f4b05b2c
Talanova <ntalanov@its.jnj.com>
2023-07-27 13:27:55 +0300

    Seminar4 Task2

---
+++ Seminar4/Task2/Program.cs
@@ -0,0 +1,14 @@
+﻿// Задача 26: Напишите программу, которая принимает на вход число и выдаёт количество цифр в числе.
+// 456 -> 3
+// 78 -> 2
+// 89126 -> 5
+
+string GetInfo(string text)
+{
+    Console.Write(text);
+    return Console.ReadLine();
+}
+
+string num = GetInfo("Введите число: ");
+int length = num.Length;
+Console.WriteLine(length);
