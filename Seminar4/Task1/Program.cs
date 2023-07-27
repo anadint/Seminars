3cac2afe208827d5044bc76da0d6f48d1f78adb4
Talanova <ntalanov@its.jnj.com>
2023-07-27 13:05:04 +0300

    Seminar4 Task1

--- Seminar4/Task1/Program.cs
+++ Seminar4/Task1/Program.cs
@@ -1,2 +1,24 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 24: Напишите программу, которая принимает на вход число (А) и выдаёт сумму чисел от 1 до А.
+// 7 -> 28
+// 4 -> 10
+// 8 -> 36
+
+int GetSum(int limit)
+{
+    int sum = 0;
+    for (int i = 1; i <= limit; i++)
+    {
+        sum += i;
+    }
+    return sum;
+}
+
+int GetInfo(string text)
+{
+    Console.Write(text);
+    int inputNum = Convert.ToInt32(Console.ReadLine());
+    return inputNum;
+}
+
+int num = GetInfo("Введите число А: ");
+Console.WriteLine($"Сумма числе от 1 до {num} равна {GetSum(num)}");
