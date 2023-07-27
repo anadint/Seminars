60535e0ee0e803ba7e48cc0a299e2e84bf6b07c4
Talanova <ntalanov@its.jnj.com>
2023-07-27 14:11:33 +0300

    Seminar4 Task4

---
+++ Seminar4/Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число N и выдаёт произведение чисел от 1 до N.
+// 4 -> 24
+// 5 -> 120
+
+int FindFactorial(int num)
+{
+    int factorial = 1;
+    while (num > 0)
+    {
+        factorial = factorial * num;
+        num--;
+    }
+    return factorial;
+}
+
+int GetInfo(string text)
+{
+    Console.Write(text);
+    int inputNum = Convert.ToInt32(Console.ReadLine());
+    return inputNum;
+}
+
+int num = GetInfo("Введите число: ");
+int factorial = FindFactorial(num);
+Console.WriteLine(factorial);
