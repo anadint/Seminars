e8bf13e7a2fac6155e7a373f6347b22554d30539
Talanova <ntalanov@its.jnj.com>
2023-07-28 00:23:17 +0300

    Seminar5 Task3

---
+++ Seminar5/Task3/Program.cs
@@ -0,0 +1,39 @@
+﻿// Задача 33: Задайте массив. Напишите программу, которая определяет, присутствует ли заданное число в массиве.
+// 4; массив [6, 7, 19, 345, 3] -> нет
+// -3; массив [6, 7, 19, 345, 3] -> да
+
+bool SearchNum(int[] array, int num)
+{
+    for (int i = 0; i < array.Length; i++)
+        if (array[i] == num || array[i] == -num)
+            return true;
+    return false;
+}
+
+int[] GenArray(int length, int minValue, int maxValue)
+{
+    int[] array = new int[length];
+
+    for (int i = 0; i < length - 1; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue);
+    }
+    return array;
+}
+
+
+int GetInfo(string text)
+{
+    Console.Write(text);
+    int inputNum = Convert.ToInt32(Console.ReadLine());
+    return inputNum;
+}
+
+int length = GetInfo("Введите длину массива: ");
+int min = GetInfo("Введите начальное значение диапазона: ");
+int max = GetInfo("Введите конечное значение диапазона: ");
+int num = GetInfo("Введите заданное число: ");
+int[] array = GenArray(length, min, max);
+Console.Write(num + "; массив [" + string.Join(",", array) + "] -> ");
+if(SearchNum(array, num)) Console.Write("да");
+else Console.Write("нет");
