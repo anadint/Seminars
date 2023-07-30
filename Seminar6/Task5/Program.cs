ed5dedba02bd989b4c385c3ac1cc035e546d5a13
Talanova <ntalanov@its.jnj.com>
2023-07-30 20:23:06 +0300

    Seminar6 Task5

---
+++ Seminar6/Task5/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 45: Напишите программу, которая будет создавать копию заданного массива с помощью поэлементного копирования.
+
+int[] GenNewArray(int[] array)
+{
+    int[] arrayNew = new int[array.Length];
+    for (int i = 0; i < array.Length; i++)
+    {
+        arrayNew[i] = array[i];
+    }
+    return arrayNew;
+}
+
+int[] GenArray(int length, int minValue, int maxValue)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+int[] array = GenArray(10, 1, 10);
+int[] arrayNew = GenNewArray(array);
+Console.WriteLine("[" + string.Join(",", array) + "] -> [" + string.Join(",", arrayNew) + "]");
