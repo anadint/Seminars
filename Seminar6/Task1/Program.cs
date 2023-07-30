464bde1f12696801a7269f13b8713acd0488f65b
Talanova <ntalanov@its.jnj.com>
2023-07-30 18:57:11 +0300

    Seminar6 Task1

--- Seminar6/Task1/Program.cs
+++ Seminar6/Task1/Program.cs
@@ -1,2 +1,36 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿// Задача 39: Напишите программу, которая перевернёт одномерный массив (последний элемент будет на первом месте, а первый - на последнем и т.д.)
+// [1 2 3 4 5] -> [5 4 3 2 1]
+// [6 7 3 6] -> [6 3 7 6]
+
+void ReversedArray(int[] array)
+{
+    for (int i = 0; i < array.Length / 2; i++)
+    {
+       int temp = array[i];
+    array[i] = array[array.Length - 1 - i];
+    array[array.Length - 1 - i] = temp; 
+    }
+}
+
+int[] GenArray(int length, int minValue, int maxValue)
+{
+    int[] array = new int[length];
+    for (int i = 0; i < length; i++)
+    {
+        array[i] = new Random().Next(minValue, maxValue + 1);
+    }
+    return array;
+}
+
+int GetInfo(string text)
+{
+    Console.Write(text);
+    int inputNum = Convert.ToInt32(Console.ReadLine());
+    return inputNum;
+}
+
+int length = GetInfo("Введите длину массива: ");
+int[] array = GenArray(length, 1, 9);
+Console.Write("[" + string.Join(",", array) + "] -> ");
+ReversedArray(array);
+Console.WriteLine("[" + string.Join(",", array) + "]");
