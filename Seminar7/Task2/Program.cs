90762935da27d57125548b93eff2acdf4af8ce69
Talanova <ntalanov@its.jnj.com>
2023-07-31 00:21:45 +0300

    Seminar7 Task2

---
+++ Seminar7/Task2/Program.cs
@@ -0,0 +1,42 @@
+﻿// Задача 48: Задайте двумерный массив размера m на n, каждый элемент в массиве находится по формуле: Aₘₙ = i+j. Выведите полученный массив на экран.
+// m = 3, n = 4.
+// 0 1 2 3
+// 1 2 3 4
+// 2 3 4 5
+
+void PrintDoubleMassive(int[,] massive)
+{
+     for (int i = 0; i < massive.GetLength(0); i++)
+    {
+        for (int j = 0; j < massive.GetLength(1); j++)
+        {
+            Console.Write($"{massive[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] DoDoubleMassive(int rows, int columns)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+          result[i, j] = i + j;  
+        }
+    }
+    return result;
+}
+
+int GetInfo(string text)
+{
+    Console.Write(text);
+    int inputNum = Convert.ToInt32(Console.ReadLine());
+    return inputNum;
+}
+
+int rows = GetInfo("Введите количество строк: ");
+int columns = GetInfo("Введите количество столбцов: ");
+int[,] massive = DoDoubleMassive(rows, columns);
+PrintDoubleMassive(massive);
