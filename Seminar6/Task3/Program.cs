46a0f04a6a5468fca467cac3b6438b61eca84fb4
Talanova <ntalanov@its.jnj.com>
2023-07-30 19:22:49 +0300

    Seminar6 Task3

---
+++ Seminar6/Task3/Program.cs
@@ -0,0 +1,25 @@
+﻿// Задача 42: Напишите программу, которая будет преобразовывать десятичное число в двоичное.
+// 45 -> 101101
+// 3 -> 11
+// 2 -> 10
+
+void DefRes(int num)
+{
+    string temp = "";
+    while (num > 0)
+    {
+        temp = (num % 2).ToString() + temp;
+        num = num / 2;
+    }
+    Console.WriteLine(temp);
+}
+
+int GetInfo(string text)
+{
+    Console.Write(text);
+    int inputNum = Convert.ToInt32(Console.ReadLine());
+    return inputNum;
+}
+
+int num = GetInfo("Введите число: ");
+DefRes(num);
