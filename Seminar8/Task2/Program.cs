45040e94e84414235b18216fe67c980214bb50d4
Talanova <ntalanov@its.jnj.com>
2023-07-31 19:20:59 +0300

    Seminar8 Task2

---
+++ Seminar8/Task2/Program.cs
@@ -0,0 +1,60 @@
+﻿// Задача 55: Задайте двумерный массив. Напишите программу, которая заменяет строки на столбцы. 
+// В случае, если это невозможно, программа должна вывести сообщение для пользователя.
+
+void PrintReversedArray(int[,] array)
+{
+    if (array.GetLength(0) == array.GetLength(1))
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            for (int i = 0; i < array.GetLength(0); i++)
+            {
+                Console.Write($"{array[i, j]} ");
+            }
+            Console.WriteLine();
+        }       
+    }
+    else
+    {     
+        Console.WriteLine("Невозможно заменить строки на столбцы");
+    }
+}
+
+void PrintArray(int[,] array)
+{
+     for (int i = 0; i < array.GetLength(0); i++)
+    {
+        for (int j = 0; j < array.GetLength(1); j++)
+        {
+            Console.Write($"{array[i,j]} ");
+        }
+        Console.WriteLine();
+    }
+}
+
+int[,] FillArray(int rows, int columns, int minValue, int maxValue)
+{
+    int[,] result = new int[rows, columns];
+    for (int i = 0; i < rows; i++)
+    {
+        for (int j = 0; j < columns; j++)
+        {
+          result[i, j] = new Random().Next(minValue, maxValue + 1);  
+        }
+    }
+    return result;
+}
+
+int GetInfo(string text)
+{
+    Console.Write(text);
+    int inputNum = Convert.ToInt32(Console.ReadLine());
+    return inputNum;
+}
+
+int rows = GetInfo("Введите количество строк: ");
+int columns = GetInfo("Введите количество столбцов: ");
+int[,] array = FillArray(rows, columns, 0, 9);
+PrintArray(array);
+Console.WriteLine();
+PrintReversedArray(array);
